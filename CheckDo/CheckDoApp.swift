042ac03a5fe8bc747ead6779709f649f9fe39633
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2023-01-16 10:43:46 -0700

    Initial Commit

---
+++ CheckDo/CheckDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckDoApp.swift
+//  CheckDo
+//
+//  Created by Jason Koehn on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CheckDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
